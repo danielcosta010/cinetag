c6719f4e597f1bcd705add7830fb7be0ce440367
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2023-02-28 00:19:47 -0300

    Favoritos

--- src/Pages/Inicio/index.js
+++ src/Pages/Inicio/index.js
@@ -1,19 +1,25 @@
+import styles  from './Inicio.module.css';
+
 import Banner from "components/Banner";
-import Cabecalho from "components/Cabecalho";
 import Card from "components/Card";
-import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
+import videos from 'json/db.json'
 
 function Inicio() {
   return (  
    <div>
-     <Cabecalho />
+     
      <Banner imagem='home'/>
      <Titulo>
       <h1>Um lugar para guardar seus vídeos e filmes</h1>
      </Titulo>
-     <Card id='1' capa='/imagens/banner-favoritos.png' titulo= 'Teste card'/>
-     <Rodape />
+     <section className={styles.container}>
+       {videos.map(video => {
+        return <Card {...video} key={video.id} class/>
+       })}
+       
+     </section>
+     
    </div>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,19 @@
+import Cabecalho from "components/Cabecalho";
+import Favoritos from "components/Favoritos";
+import Rodape from "components/Rodape";
 import Inicio from "Pages/Inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function AppRoutes() {
   return (  
     <BrowserRouter>
+      <Cabecalho />
       <Routes>
-        <Route path="/" element={<Inicio /> }></Route>
+        <Route path="/" element={<Inicio /> } />
+        <Route path="/favoritos" element={<Favoritos /> } />
         
       </Routes>
+      <Rodape />
     </BrowserRouter>
   );
 }
